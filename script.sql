06075809e5b704003bf5c6fc503b6d0060ed69ea
javier <javier@192.168.0.23>
2016-05-02 16:03:16 -0600

    script nuevo
    script nuevo definitivo

--- script.sql
+++ script.sql
@@ -1,64 +1,51 @@
-CREATE TABLE Clientes
-(
-	costumer_id          NUMBER NOT NULL ,
-	cust_first_name      VARCHAR2(30) NULL ,
-	cust_last_name       VARCHAR2(30) NULL 
-);
 
-ALTER TABLE Clientes
-	ADD CONSTRAINT  XPKClientes PRIMARY KEY (costumer_id);
-
-CREATE TABLE Productos
+CREATE TABLE Materia
 (
 	product_id           NUMBER NOT NULL ,
 	product_name         VARCHAR2(30) NULL ,
-	product_description  VARCHAR2(40) NULL ,
-	buy_price            NUMBER NULL ,
-	sell_price           NUMBER NULL 
+	buy_price            NUMBER NULL 
 );
 
-ALTER TABLE Productos
+ALTER TABLE Materia
 	ADD CONSTRAINT  XPKProductos PRIMARY KEY (product_id);
 
-CREATE TABLE Inventario
+CREATE TABLE Sucursal
+(
+	suc_id               NUMBER NOT NULL ,
+	suc_nom              VARCHAR2(30) NULL ,
+	suc_dir              VARCHAR2(40) NULL ,
+	suc_tel              NUMBER NULL 
+);
+
+ALTER TABLE Sucursal
+	ADD CONSTRAINT  XPKSucursal PRIMARY KEY (suc_id);
+
+CREATE TABLE Materia_Sucursal
 (
 	product_id           NUMBER NOT NULL ,
+	suc_id               NUMBER NOT NULL ,
 	quantity_on_hand     NUMBER NULL 
 );
 
-ALTER TABLE Inventario
-	ADD CONSTRAINT  XPKInventario PRIMARY KEY (product_id);
+ALTER TABLE Materia_Sucursal
+	ADD CONSTRAINT  XPKProductos_Sucursal PRIMARY KEY (product_id,suc_id);
 
-CREATE TABLE Proveedores
+CREATE TABLE Clientes
 (
-	prov_id              NUMBER NOT NULL ,
-	prov_name            VARCHAR2(30) NULL ,
-	prov_address         VARCHAR2(30) NULL ,
-	prov_phone           NUMBER NULL ,
-	product_id           NUMBER NULL 
+	customer_id          NUMBER NOT NULL ,
+	cust_first_name      VARCHAR(30) NULL ,
+	cust_last_name       VARCHAR(30) NULL ,
+	suc_id               NUMBER NULL 
 );
 
-ALTER TABLE Proveedores
-	ADD CONSTRAINT  XPKProveedores PRIMARY KEY (prov_id);
+ALTER TABLE Clientes
+	ADD CONSTRAINT  XPKClientes PRIMARY KEY (customer_id);
 
-CREATE TABLE Clientes_Productos
-(
-	costumer_id          NUMBER NOT NULL ,
-	product_id           NUMBER NOT NULL ,
-	quantity_prod        NUMBER NOT NULL 
-);
+ALTER TABLE Materia_Sucursal
+	ADD (CONSTRAINT R_58 FOREIGN KEY (product_id) REFERENCES Materia (product_id));
 
-ALTER TABLE Clientes_Productos
-	ADD CONSTRAINT  XPKClientes_Productos PRIMARY KEY (costumer_id,product_id);
+ALTER TABLE Materia_Sucursal
+	ADD (CONSTRAINT R_59 FOREIGN KEY (suc_id) REFERENCES Sucursal (suc_id));
 
-ALTER TABLE Inventario
-	ADD (CONSTRAINT R_11 FOREIGN KEY (product_id) REFERENCES Productos (product_id));
-
-ALTER TABLE Proveedores
-	ADD (CONSTRAINT R_16 FOREIGN KEY (product_id) REFERENCES Productos (product_id) ON DELETE SET NULL);
-
-ALTER TABLE Clientes_Productos
-	ADD (CONSTRAINT R_31 FOREIGN KEY (costumer_id) REFERENCES Clientes (costumer_id));
-
-ALTER TABLE Clientes_Productos
-	ADD (CONSTRAINT R_32 FOREIGN KEY (product_id) REFERENCES Productos (product_id));
+ALTER TABLE Clientes
+	ADD (CONSTRAINT R_60 FOREIGN KEY (suc_id) REFERENCES Sucursal (suc_id) ON DELETE SET NULL);
